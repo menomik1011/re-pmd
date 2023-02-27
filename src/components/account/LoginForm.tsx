5fb45c34c9ce5e2543a03d3c29dec03fbf20fb9b
menomik1011 <meno.mik1011@gmail.com>
2023-02-27 12:52:39 +0900

    add login css

--- src/components/account/LoginForm.tsx
+++ src/components/account/LoginForm.tsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
+import styles from "./LoginForm.module.css";
 export default function LoginForm() {
   return (
-    <div>LoginForm</div>
-  )
+    <div className={styles.Wrapper}>
+      <h1 className={styles.FormTitle}>Sign In</h1>
+      <form className={styles.Form}>
+        <div className={styles.Label_div}>
+          <label>Username</label>
+        </div>
+        <input type={"text"} />
+        <div>
+          <div className={styles.Label_div}>
+            <label>Password</label>
+          </div>
+          <input type={"text"} />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+      <Link className={styles.link} href="join">Create Account</Link>
+    </div>
+  );
 }
