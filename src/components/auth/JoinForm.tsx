8c2ad2e123d08de3a899de4a08383de319037b5d
menomik1011 <meno.mik1011@gmail.com>
2023-02-27 14:18:29 +0900

    add home link

--- src/components/auth/JoinForm.tsx
+++ src/components/auth/JoinForm.tsx
@@ -3,7 +3,10 @@ import styles from "./Form.module.css";
 
 export default function JoinForm() {
   return (
-    <>
+    <main className={styles.Main}>
+      <header className={styles.Header}>
+        <Link href="/">Home</Link>
+      </header>
       <div className={styles.Wrapper}>
         <h1 className={styles.FormTitle}>Sign Up</h1>
         <form className={styles.Form}>
@@ -28,9 +31,10 @@ export default function JoinForm() {
           <button type="submit">Create an Account</button>
         </form>
       </div>
-      <Link className={styles.link} href="login">
+      <p className={styles.Alert}>Already have an account?</p>
+      <Link className={styles.Link} href="login">
         Sign In
       </Link>
-    </>
+    </main>
   );
 }
--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -2,26 +2,31 @@ import Link from "next/link";
 import styles from "./Form.module.css";
 export default function LoginForm() {
   return (
-    <div className={styles.Wrapper}>
-      <h1 className={styles.FormTitle}>Sign In</h1>
-      <form className={styles.Form}>
-        <div>
-          <div className={styles.Label_div}>
-            <label>Username</label>
+    <main className={styles.Main}>
+      <header className={styles.Header}>
+        <Link href="/">Home</Link>
+      </header>
+      <div className={styles.Wrapper}>
+        <h1 className={styles.FormTitle}>Sign In</h1>
+        <form className={styles.Form}>
+          <div>
+            <div className={styles.Label_div}>
+              <label>Username</label>
+            </div>
+            <input type="text" />
           </div>
-          <input type="text" />
-        </div>
-        <div>
-          <div className={styles.Label_div}>
-            <label>Password</label>
+          <div>
+            <div className={styles.Label_div}>
+              <label>Password</label>
+            </div>
+            <input type="password" />
           </div>
-          <input type="password" />
-        </div>
-        <button type="submit">Sign In</button>
-      </form>
-      <Link className={styles.link} href="join">
-        Create Account
-      </Link>
-    </div>
+          <button type="submit">Sign In</button>
+        </form>
+        <Link className={styles.Link} href="join">
+          Create Account
+        </Link>
+      </div>
+    </main>
   );
 }
