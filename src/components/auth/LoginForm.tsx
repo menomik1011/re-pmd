ebd176b629d55a93149c8783084389ed278a01f3
menomik1011 <meno.mik1011@gmail.com>
2023-02-28 11:46:52 +0900

    add loginForm 관심사 분리 + type 정리

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,6 +1,22 @@
 import Link from "next/link";
+import { ChangeEvent, FormEvent } from "react";
 import styles from "./Form.module.css";
-export default function LoginForm() {
+
+export type LoginFormProps = {
+  username: string;
+  password: string;
+  usernameOnChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  passwordOnChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  loginOnSubmit: (e: FormEvent<HTMLFormElement>) => void;
+};
+
+export default function LoginForm({
+  username,
+  password,
+  usernameOnChange,
+  passwordOnChange,
+  loginOnSubmit,
+}: LoginFormProps) {
   return (
     <main className={styles.Main}>
       <header className={styles.Header}>
@@ -8,18 +24,22 @@ export default function LoginForm() {
       </header>
       <div className={styles.Wrapper}>
         <h1 className={styles.FormTitle}>Sign In</h1>
-        <form className={styles.Form}>
+        <form className={styles.Form} onSubmit={loginOnSubmit}>
           <div>
             <div className={styles.Label_div}>
               <label>Username</label>
             </div>
-            <input type="text" />
+            <input type="text" value={username} onChange={usernameOnChange} />
           </div>
           <div>
             <div className={styles.Label_div}>
               <label>Password</label>
             </div>
-            <input type="password" />
+            <input
+              type="password"
+              value={password}
+              onChange={passwordOnChange}
+            />
           </div>
           <button type="submit">Sign In</button>
         </form>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,27 @@
-import LoginForm from "@/components/auth/LoginForm";
+import LoginForm, { LoginFormProps } from "@/components/auth/LoginForm";
+import { useState } from "react";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const usernameOnChange: LoginFormProps["usernameOnChange"] = (e) => {
+    setUsername(e.target.value);
+  };
+  const passwordOnChange: LoginFormProps["passwordOnChange"] = (e) => {
+    setPassword(e.target.value);
+  };
+  const loginOnSubmit: LoginFormProps["loginOnSubmit"] = (e) => {
+    e.preventDefault();
+    console.log({ name: username, password: password });
+  };
   return (
-    <LoginForm />
-  )
+    <LoginForm
+      username={username}
+      password={password}
+      usernameOnChange={usernameOnChange}
+      passwordOnChange={passwordOnChange}
+      loginOnSubmit={loginOnSubmit}
+    />
+  );
 }
